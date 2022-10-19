4dab3552d148b138d685f5aaa7572b5de1c9c4bf
Lars T Hansen <lars.t.hansen@gmail.com>
2022-10-19 09:43:54 +0200

    WIP

--- runs/runs.go
+++ runs/runs.go
@@ -4,7 +4,10 @@
 //     them in decreasing frequency order.
 //
 // Options:
+//   -r    Consider runs to be non-overlapping
 //   -t n  Print only the top n.  Default=100.  Zero means 'all'
+//   -b    Print the bottom n instead of the top n
+//   -c m  Cutoff for consideration.  Default=0.
 //
 // TODO: Generalize this to n-byte runs for n up to at least eight.
 
@@ -30,8 +33,12 @@ func (ft stats) Less(i, j int) bool { return ft[i].count > ft[j].count }
 func (ft stats) Swap(i, j int)      { ft[i], ft[j] = ft[j], ft[i] }
 
 func main() {
-	var tFlag uint
+	var tFlag, cFlag uint
+	var rFlag, bFlag bool
 	flag.UintVar(&tFlag, "t", 100, "Number of entries to list")
+	flag.UintVar(&cFlag, "c", 0, "Cutoff for useless values")
+	flag.BoolVar(&rFlag, "r", false, "Consider entries not to overlap")
+	flag.BoolVar(&bFlag, "b", false, "Print the bottom entries instead")
 	flag.Parse()
 	if tFlag == 0 {
 		tFlag = 65536
@@ -49,20 +56,32 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		for i := 0; i < bytesRead-1; i++ {
+		inc := 1
+		if rFlag {
+			inc = 2
+		}
+		for i := 0; i < bytesRead-1; i += inc {
 			n := (uint(buf[i]) << 8) | uint(buf[i+1])
 			ss[n].count++
 		}
 	}
 	sort.Sort(ss)
-	var numNonblank int
-	for numNonblank = len(ss); numNonblank > 0 && ss[numNonblank-1].count == 0; numNonblank-- {
+	var numUseful int
+	for numUseful = len(ss); numUseful > 0 && ss[numUseful-1].count <= cFlag; numUseful-- {
 	}
-	if tFlag > uint(numNonblank) {
-		tFlag = uint(numNonblank)
+	if tFlag > uint(numUseful) {
+		tFlag = uint(numUseful)
 	}
-	for _, v := range ss[:tFlag] {
+	toPrint := ss[:tFlag]
+	if bFlag {
+		toPrint = ss[uint(numUseful)-tFlag : numUseful]
+	}
+	for _, v := range toPrint {
 		fmt.Fprintf(os.Stdout, "%04x\t%d\n", v.val, v.count)
 	}
-	fmt.Fprintf(os.Stdout, "Number of nonzero entries: %d\n", numNonblank)
+	overlap := ""
+	if rFlag {
+		overlap = "non-"
+	}
+	fmt.Fprintf(os.Stdout, "Number of useful %soverlapping entries: %d\n", overlap, numUseful)
 }
