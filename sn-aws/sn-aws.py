c8f8ed56ff27761284e837f0e8b693214cbc0954
Lars T Hansen <lars.t.hansen@gmail.com>
2022-11-23 09:54:53 +0100

    Works

--- sn-aws/sn-aws.py
+++ sn-aws/sn-aws.py
@@ -4,7 +4,7 @@
 
 # Usage:
 #   sn up filename    // Filename can have path delimiters
-#   sn down filename  // Filename can have no path delimiters
+#   sn down filename [dest] // Filename can have no path delimiters but dest can
 #   sn ls pattern     // Pattern can have no path delimiters, and is prefix-only?
 
 import os
@@ -12,8 +12,9 @@ import re
 import sys
 
 def doit(cmd):
-    sys.stdout.write(cmd + "\n")
-
+    # sys.stdout.write(cmd + "\n")
+    os.system(cmd)
+    
 def up():
     global AWS_PREFIX
     if len(sys.argv) < 3:
@@ -23,6 +24,8 @@ def up():
     assert_file_exists(fn)
     # FIXME: Should allow path name in the file, but should be reduced to base name in the
     # destination
+    #
+    # FIXME: Could allow destination to be specified separately
     doit(f"aws s3 cp {fn} {AWS_PREFIX}{fn}")
 
 def down():
@@ -30,11 +33,12 @@ def down():
     if len(sys.argv) < 3:
         usage()
     fn = sys.argv[2]
+    dest = fn if len(sys.argv) < 4 else sys.argv[3]
     assert_valid_filename(fn)
-    assert_file_not_exists(fn)
+    assert_file_not_exists(dest)
     # FIXME: Should allow path name in the file, but should be reduced to the base name in
     # the source?  ie, sn down a/b/c gets "c" from the server and stores it locally at a/b/
-    doit(f"aws s3 cp {AWS_PREFIX}{fn} .")
+    doit(f"aws s3 cp {AWS_PREFIX}{fn} {dest}")
 
 def ls():
     global AWS_PREFIX
@@ -43,30 +47,30 @@ def ls():
 
 def assert_valid_filename(fn):
     if not re.compile(r"^[-a-zA-Z0-9._]+$").match(fn):
-        sys.stderr.write(f"Bad file name {fn}, must match /^[-a-zA-Z0-9._]+$/")
-        sys.exit(1)
+        fail(f"Bad file name {fn}, must match /^[-a-zA-Z0-9._]+$/")
 
 def assert_file_exists(fn):
     if not os.access(fn, os.R_OK):
-        sys.stderr.write(f"File not found: `{fn}`\n")
-        sys.exit(1)
+        fail(f"File not found: `{fn}`")
 
 def assert_file_not_exists(fn):
     if os.access(fn, os.F_OK):
-        sys.stderr.write(f"File already exists: `{fn}`\n")
-        sys.exit(1)
+        fail(f"File already exists: `{fn}`")
 
 def usage():
     sys.stderr.write(
 """Usage: sn up filename
-       sn down pattern
+       sn down filename [dest]
        sn ls [pattern]
 """)
     sys.exit(1)
 
-if not "AWS_SN_BUCKET" in os.environ:
-    sys.stderr.write("No value for AWS_SN_BUCKET in environment")
+def fail(msg):
+    sys.stderr.write(msg + "\n")
     sys.exit(1)
+
+if not "AWS_SN_BUCKET" in os.environ:
+    fail("No value for AWS_SN_BUCKET in environment")
 
 AWS_PREFIX = f"s3://{os.environ['AWS_SN_BUCKET']}/TRANSIT/"
 
