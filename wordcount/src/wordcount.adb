6795e33be7873ed5d03446c5f86e6d9e0076f6a5
Lars T Hansen <lars.t.hansen@gmail.com>
2022-12-01 11:38:45 +0100

    Playing around some more

--- wordcount/src/wordcount.adb
+++ wordcount/src/wordcount.adb
@@ -1,37 +1,73 @@
 --  -*- indent-tabs-mode: nil -*-
+--
+--  Usage: wordcount [input-file]
+--    Reads from stdin if there is no input-file
 
-with Ada.Text_IO;
-with Ada.Strings.Maps;
 with Ada.Characters.Latin_1;
+with Ada.Command_Line;
+with Ada.Strings.Maps;
+with Ada.Text_IO;
 
 procedure Wordcount is
-   package Integer_Text_IO is new Ada.Text_IO.Integer_IO (Integer);
+   package Nat_Text_IO is new Ada.Text_IO.Integer_IO (Natural);
 
-   use Ada.Text_IO;
    use Ada.Characters;
+   use Ada.Command_Line;
    use Ada.Strings.Maps;
-   use Integer_Text_IO;
+   use Ada.Text_IO;
+   use Nat_Text_IO;
 
    type State_Type is (Outside, Inside);
 
-   f : File_Type;
-   Chars : Integer := 0;
-   Words : Integer := 0;
-   Lines : Integer := 0;
-   Whitespace : constant Character_Set := "or" (To_Set (Latin_1.Space), To_Set (Latin_1.HT));
+   Named_File : File_Type;
+   Chars : Natural := 0;
+   Words : Natural := 0;
+   Lines : Natural := 0;
+   Has_File : Boolean := False;
+   Whitespace : constant Character_Set :=
+         To_Set (Latin_1.Space) or
+         To_Set (Latin_1.LF) or
+         To_Set (Latin_1.FF) or
+         To_Set (Latin_1.CR) or
+         To_Set (Latin_1.HT) or
+         To_Set (Latin_1.VT);
 
    --  To match `wc`, a character is in a word if it is not whitespace.
+   --  Not actually sure the whitespace set above completely matches, but
+   --  it should be OK.
 
    function Is_Inside_Char (c : Character) return Boolean is
    begin
       return (not Is_In (c, Whitespace));
    end Is_Inside_Char;
 
+   --  At_Eof and Read_Line appear to be necessary since I can't take the
+   --  address of Named_File to unify named-file with Standard_Input.
+
+   function At_Eof return Boolean is
+   begin
+      if Has_File then
+         return End_Of_File (Named_File);
+      end if;
+      return End_Of_File (Standard_Input.all);
+   end At_Eof;
+
+   function Read_Line return String is
+   begin
+      if Has_File then
+         return Get_Line (Named_File);
+      end if;
+      return Get_Line (Standard_Input.all);
+   end Read_Line;
+
 begin
-   Open (f, In_File, "test.txt");
-   while not End_Of_File (f) loop
+   if Argument_Count >= 1 then
+      Open (Named_File, In_File, Argument (1));
+      Has_File := True;
+   end if;
+   while not At_Eof loop
       declare
-         s : constant String := Get_Line (f);
+         s : constant String := Read_Line;
          State : State_Type := Outside;
       begin
          Lines := Lines + 1;
@@ -50,5 +86,7 @@ begin
    Put (Words);
    Put (Chars);
    New_Line;
-   Close (f);
+   if Has_File then
+      Close (Named_File);
+   end if;
 end Wordcount;
