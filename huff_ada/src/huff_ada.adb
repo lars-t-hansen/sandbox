c0c0b974a8407ceb99cf76a531f5039185aa543f
Lars T Hansen <lars.t.hansen@gmail.com>
2022-12-06 15:10:16 +0100

    Cleanup

--- huff_ada/src/huff_ada.adb
+++ huff_ada/src/huff_ada.adb
@@ -2,20 +2,49 @@
 
 --  Huffman encoder, in Ada.
 --
---  For a spec of the output, see any of the Huffman encoders written in other
---  languages below the parent directory.
+--  Usage:
+--     huff [-o output-file] input-file
 --
---  This program has only the encoding part, and currently only single-threaded.
+--  If there's no -o argument then the output file is input-file.huff.
+--
+--  Note, this program has only the encoding part, and currently only single-threaded.
+--
+--  TODO: Add at least a multithreaded encoder.
+--
+--  Since this is a programming exercise, it works by reading 64KB blocks and
+--  compressing them individually; the output file consists of compressed blocks.
+--  Also, we don't care about micro-efficiencies in representing the dictionary
+--  in the file or in complicated fallback schemes, more could be done.
+-- 
+--  A compressed block is represented as
+--    number of dictionary entries: u16 > 0 (max value is really 256)
+--    run of dictionary entries sorted descending by frequency with ties
+--    broken by lower byte values first:
+--      value: u8
+--      frequency: u32 (max value is really 65536)
+--    number of encoded bytes: u32 (max value is really 65536)
+--    number of bytes used for encoded bytes: u32 (max value 65536)
+--    bytes, the number of which is encoded by previous field
+-- 
+--  An uncompressed block can be written under some circumstances, it is represented as
+--    0: u16
+--    number of bytes: u32 (really max 65536)
+--    bytes, the number of which is encoded by previous field
+--
+-- The format has some problems; see the comment block in the Rust version.
 
 with Interfaces; use Interfaces;
 with Heap;
+with Ada.Command_Line;
 with Ada.Containers.Generic_Constrained_Array_Sort;
 with Ada.Sequential_IO;
+with Ada.Text_IO;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 procedure Huff_Ada is
 
    --  Cant_Encode is raised during encoding if internal limits are exceeded
-   --  (bit string width; encoded size).  When raised, the encoder should just
+   --  (bit string width; encoded size).  When raised, the compressor should just
    --  dump the input block verbatim on the output.
 
    Cant_Encode : exception;
@@ -23,8 +52,6 @@ procedure Huff_Ada is
 
    --  Block used to represent a byte sequence with a buffer and a varying
    --  length.
-   --
-   --  TODO: It's possible that the array should be indirect.
 
    type Buffer_Array is array (0 .. 65535) of Unsigned_8;
 
@@ -53,7 +80,7 @@ procedure Huff_Ada is
       Length : Freq_Array_Length;
       It     : Freq_Array;
    end record;
-   
+ 
    procedure Compute_Frequencies(Input : Buffer; Freqs : out Freq_Table) is
 
       function "<" (a, b: Freq_Item) return Boolean is
@@ -61,7 +88,8 @@ procedure Huff_Ada is
          return (a.Count > b.Count) or else (a.Count = b.Count and then a.Ch < b.Ch);
       end "<";
 
-      procedure Sort_Freq is new Ada.Containers.Generic_Constrained_Array_Sort (Freq_Array_Range, Freq_Item, Freq_Array);
+      procedure Sort_Freq is new Ada.Containers.
+         Generic_Constrained_Array_Sort (Freq_Array_Range, Freq_Item, Freq_Array);
 
    begin
       pragma Assert (Input.Length > 0);
@@ -106,7 +134,8 @@ procedure Huff_Ada is
 
    function ">"(Left, Right : Huff_Item) return Boolean is
    begin
-      return (Left.Weight < Right.Weight) or else (Left.Weight = Right.Weight and then Left.Serial < Right.Serial);
+      return (Left.Weight < Right.Weight) or else
+             (Left.Weight = Right.Weight and then Left.Serial < Right.Serial);
    end ">";
 
    procedure Build_Huffman_Tree(Freqs : Freq_Table; Tree : out Huff_Node_Ptr) is
@@ -122,7 +151,9 @@ procedure Huff_Ada is
          declare
             ix : constant Freq_Array_Range := Freq_Array_Range (i);
          begin
-            HuffHeap.Insert (Priq, Huff_Item'(Freqs.It (ix).Count, Serial, new Huff_Node'(Freqs.It (ix).Ch, null, null)));
+            HuffHeap.Insert (Priq, Huff_Item'(Freqs.It (ix).Count,
+                                              Serial,
+                                              new Huff_Node'(Freqs.It (ix).Ch, null, null)));
             Serial := Serial + 1;
          end;
       end loop;
@@ -132,7 +163,9 @@ procedure Huff_Ada is
          begin
             HuffHeap.Extract_Max (Priq, a);
             HuffHeap.Extract_Max (Priq, b);
-            HuffHeap.Insert (Priq, Huff_Item'(a.Weight + b.Weight, Serial, new Huff_Node'(0, a.Tree, b.Tree)));
+            HuffHeap.Insert (Priq, Huff_Item'(a.Weight + b.Weight,
+                                              Serial,
+                                              new Huff_Node'(0, a.Tree, b.Tree)));
             Serial := Serial + 1;
          end;
       end loop;
@@ -176,7 +209,6 @@ procedure Huff_Ada is
             Dict (Dict_Range (Tree.Ch)).Width := Width;
             Dict (Dict_Range (Tree.Ch)).Bits := Bits;
          else
-            pragma Assert (Tree.Ch = 0);
             Descend(Tree.Left, Bits, Width + 1);
             Descend(Tree.Right, Shift_Left(1, Width) or Bits, Width + 1);
          end if;
@@ -201,10 +233,7 @@ procedure Huff_Ada is
       Width  : Natural := 0;
       dix    : Dict_Range;
    begin
-      loop
-         if Inptr = Input.Length then
-            exit;
-         end if;
+      while Inptr < Input.Length loop
          dix := Dict_Range (Input.It (Inptr));
          Bits := Bits or Shift_Left(Dict (dix).Bits, Width);
          Width := Width + Dict (dix).Width;
@@ -240,60 +269,60 @@ procedure Huff_Ada is
       It     : Metadata;
    end record;
 
-   procedure Put1(A : in out MetaBuffer; V : Unsigned_64) is
+   procedure Put_8(A : in out MetaBuffer; V : Unsigned_8) is
    begin
-      A.It (A.Length) := Unsigned_8 (V);
+      A.It (A.Length) := V;
       A.Length := A.Length + 1;
-   end Put1;
+   end Put_8;
 
-   procedure Put2(A : in out MetaBuffer; V : Unsigned_64) is
+   procedure Put_16(A : in out MetaBuffer; V : Unsigned_16) is
    begin
       A.It (A.Length) := Unsigned_8 (V and 255);
       A.It (A.Length + 1) := Unsigned_8 (Shift_Right(V, 8) and 255);
       A.Length := A.Length + 2;
-   end Put2;
+   end Put_16;
 
-   procedure Put4(A : in out MetaBuffer; V : Unsigned_64) is
+   procedure Put_32(A : in out MetaBuffer; V : Unsigned_32) is
    begin
       A.It (A.Length) := Unsigned_8 (V and 255);
       A.It (A.Length + 1) := Unsigned_8 (Shift_Right(V, 8) and 255);
       A.It (A.Length + 2) := Unsigned_8 (Shift_Right(V, 16) and 255);
       A.It (A.Length + 3) := Unsigned_8 (Shift_Right(V, 24) and 255);
       A.Length := A.Length + 4;
-   end Put4;
+   end Put_32;
 
 
    -- Block compressor.
    --
-   -- When Compress_Block returns normally, the Output and the Metadata together
-   -- hold the compressed representation.
-   --
-   -- Compress_Block does not raise any exceptions.
+   -- When Compress_Block returns, the Output and the Metadata together
+   -- hold the compressed representation.  No exceptions are raised.
 
    procedure Compress_Block (Input : Buffer; Output : out Buffer; Meta : out MetaBuffer) is
       Freqs : Freq_Table;
       Dict  : Dictionary;
       Tree  : Huff_Node_Ptr;
+
    begin
       Compute_Frequencies (Input, Freqs);
       Build_Huffman_Tree (Freqs, Tree);
       Build_Dictionary (Tree, Dict);
+      Encode_Block (Input, Output, Dict);
       Meta.Length := 0;
-      begin
-         Encode_Block (Input, Output, Dict);
-         Put2 (Meta, Unsigned_64 (Freqs.Length));
-         for i in 0 .. Freqs.Length-1 loop
-            Put1 (Meta, Unsigned_64 (Freqs.It (Freq_Array_Range (i)).Ch));
-            Put4 (Meta, Unsigned_64 (Freqs.It (Freq_Array_Range (i)).Count));
-         end loop;
-         Put4 (Meta, Unsigned_64 (Input.Length));
-      exception
-         when Cant_Encode =>
-            Output.It (0 .. Input.Length-1) := Input.It (0 .. Input.Length-1);
-            Output.Length := Input.Length;
-            Put2 (Meta, 0);
-      end;
-      Put4 (Meta, Unsigned_64 (Output.Length));
+      Put_16 (Meta, Unsigned_16 (Freqs.Length));
+      for i in 0 .. Freqs.Length-1 loop
+         Put_8 (Meta, Freqs.It (Freq_Array_Range (i)).Ch);
+         Put_32 (Meta, Unsigned_32 (Freqs.It (Freq_Array_Range (i)).Count));
+      end loop;
+      Put_32 (Meta, Unsigned_32 (Input.Length));
+      Put_32 (Meta, Unsigned_32 (Output.Length));
+
+   exception
+      when Cant_Encode =>
+         Meta.Length := 0;
+         Output.It (0 .. Input.Length-1) := Input.It (0 .. Input.Length-1);
+         Output.Length := Input.Length;
+         Put_16 (Meta, 0);
+         Put_32 (Meta, Unsigned_32 (Output.Length));
    end Compress_Block;
 
 
@@ -310,7 +339,7 @@ procedure Huff_Ada is
       FIO.Open(Input_File, FIO.In_File, Input_Name);
       FIO.Create(Output_File, FIO.Out_File, Output_Name);
       while not FIO.End_Of_File(Input_File) loop
-         --  This is completely tragic.  Surely there has got to be a better way than
+         --  TODO: This is completely tragic.  Surely there has got to be a better way than
          --  byte-at-a-time?
          for i in 0 .. Input.It'Last loop
             if FIO.End_Of_File(Input_File) then
@@ -320,7 +349,7 @@ procedure Huff_Ada is
             FIO.Read(Input_File, Input.It (i));
          end loop;
          Compress_Block (Input, Output, Meta);
-         --  Ditto.
+         --  TODO: Ditto.
          for i in 0 .. Meta.Length-1 loop
             FIO.Write(Output_File, Meta.It (i));
          end loop;
@@ -332,12 +361,55 @@ procedure Huff_Ada is
       FIO.Close(Input_File);
    end Compress_File;
 
-begin
-   --  TODO: Grab file names from the command line.
-   declare
-      Input_Name  : String := "test.txt";
-      Output_Name : String := "test.txt.huff";
+   procedure Parse_Arguments (Input_Name, Output_Name: in out Unbounded_String;
+                              Failed : in out Boolean) is
+      use Ada.Command_Line;
+      use Ada.Text_IO;
+
+      Next_Arg : Natural := 1;
+      Explicit_Output : Boolean := False;
    begin
-      Compress_File (Input_Name, Output_Name);
-   end;
+      if Next_Arg <= Argument_Count and then Argument (Next_Arg) = "-o" then
+         Next_Arg := Next_Arg + 1;
+         if Next_Arg <= Argument_Count then
+            Output_Name := To_Unbounded_String(Argument (Next_Arg));
+            Explicit_Output := True;
+            Next_Arg := Next_Arg + 1;
+         else
+            --  Output file required after -o
+            goto Borked;
+         end if;
+      end if;
+      if Next_Arg <= Argument_Count then
+         Input_Name := To_Unbounded_String(Argument (Next_Arg));
+         Next_Arg := Next_Arg + 1;
+      else
+         --  Input file required
+         goto Borked;
+      end if;
+      if Next_Arg <= Argument_Count then
+         --  Unconsumed arguments
+         goto Borked;
+      end if;
+      if not Explicit_Output then
+         Output_Name := Input_Name & ".huff";
+      end if;
+      return;
+
+   <<Borked>>
+      Put_Line ("Usage: huff [-o output-file] input-file");
+      Set_Exit_Status (1);
+      Failed := True;
+   end Parse_Arguments;
+
+   Failed : Boolean := False;
+   Input_Name : Unbounded_String;
+   Output_Name : Unbounded_String;
+
+begin
+   Parse_Arguments (Input_Name, Output_Name, Failed);
+   if Failed then
+      return;
+   end if;
+   Compress_File (To_String(Input_Name), To_String(Output_Name));
 end Huff_Ada;
