8e837f244dfb68b699664fb5933be253a52da903
Lars T Hansen <lars.t.hansen@gmail.com>
2022-12-14 09:16:08 +0100

    Dink dink

--- math-professor/lambda_function.py
+++ math-professor/lambda_function.py
@@ -1,26 +1,32 @@
 # -*- fill-column: 100 -*-
 
-# This is just an experiment to play around with AWS and Python, it doesn't amount to anything.
+# This is just an experiment to play around with AWS Lambda, AWS IoT, and Python, it doesn't amount
+# to anything.
 
 import math
 import boto3
-from mqtt_stuff import *
+import mqtt_stuff
 
 # An AWS IoT MQTT rule transforms this message
 #
-#    topic:   math/function/question
-#    payload: {"arg":something}
+#    topic:   math/<function>/question
+#    payload: {"arg": something}
 #
 # into this event for the below code to process:
 #
-#    event:   {"function": function, "arg": something}
+#    event:   {"function": function,
+#              "arg":      something}
 #
 # The output from the present program is posted by MQTT:
 #
-#    topic:   math/function/answer
-#    payload: {"function": function, "input": arg, "output": result}
+#    topic:   math/<function>/answer
+#    payload: {"function": function,
+#              "input":    arg,
+#              "output":   result,
+#              "found":    boolean}
 #
-# Supported functions are "fib", "fact", and "sin"
+# Supported functions are "fib", "fact", and "sin".  Results are cached per function and input;
+# "found" is True if a cached result was returned.
 
 def lambda_handler(event, context):
     function = event["function"]
@@ -44,11 +50,11 @@ def lambda_handler(event, context):
     if not found:
         db.put_item(TableName='math_data', Item={**key, "output":{"N":str(result)}})
 
-    publish(f"math/{function}/answer",
-            {"function": function,
-             "input":    arg,
-             "output":   result,
-             "found":    found})
+    mqtt_stuff.publish(f"math/{function}/answer",
+                       {"function": function,
+                       "input":    arg,
+                       "output":   result,
+                       "found":    found})
 
 def fib(n):
     if n < 2:
