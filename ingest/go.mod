b34e8b29eb729c4d010b41528592be613cbde1f2
Lars T Hansen <larstha@uio.no>
2025-03-10 08:16:18 +0100

    Experiment

---
+++ ingest/go.mod
@@ -0,0 +1,5 @@
+module ingest
+
+go 1.23.6
+
+require github.com/NordicHPC/sonar/util/formats v0.0.0-000101010000-000000000000
---
+++ ingest/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/NordicHPC/sonar/util/formats/oldfmt"
+)
+
+func main() {
+	var v *oldfmt.SysinfoEnvelope
+	fmt.Println(v)
+}
