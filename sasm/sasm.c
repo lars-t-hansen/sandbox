45cb904c78200c209cbe1b275c1e12c68e34a972
Lars T Hansen <larstha@uio.no>
2026-04-16 12:55:33 +0200

    Dink

--- sasm/sasm.c
+++ sasm/sasm.c
@@ -1,5 +1,8 @@
-// suppose we want a maximally simple assembler for a subset of z80 (a simple case).  we could
-// simplify further by using 8080... but not yet.  this would have syntax that is very restricted:
+// suppose we want a maximally simple assembler for a subset of z80 (a simple case).  we should
+// simplify further by using 8080 assembly, which has a much simpler syntax and instruction layout.
+// execpt for MOV, everything has at most one operand and there is at most one opcode byte.
+//
+// Meaningful restrictions:
 //
 // all upper (or lower) case mandated
 // all 7-bit ascii
@@ -31,30 +34,27 @@
 //
 // Eventually this assembler will be written in its own language so no cheating with regexes.  All values are 8
 // or 16 bits, signed or unsigned.
-//
-// For a lot of instructions we could define our own opcodes, eg ADD A, ... is ADDA while ADD HL,
-// ... is ADDHL, AND (HL) could be ANDI.  We ignore IX, IY, this changes a lot.  ADD A, r could be
-// ADDAR r while ADD A, n could be ADDAN.
-//
-// Probably 8080 is exactly what we need: execpt for MOV, everything has at most one operand and there
-// is at most one opcode byte.
-//
-// use 'r' for 8-bit regs, 'w' for 16-bit regs.
 
-// Every line is terminated by *, end of input also.  If nothing is found, nothing is output.
-void scan_line(char* p) {
+/* The input is the entire input file.  Every line is terminated by \n, end of input by 0x00. */
+/* Labels and names are max five chars.  There's no output overflow check. */
+
+char w[6];                      /* current word */
+char lbl[2];                    /* label on this line if first char is not ' ' */
+char* w2;                       /* word to compare to */
+char* p;                        /* input pointer */
+char* q;                        /* output pointer */
+
+void scan() {
     char c = *p++;
-    char word[6];
-    char label[2];
-newword:
-    word[0] = ' ';
-    word[1] = ' ';
-    word[2] = ' ';
-    word[3] = ' ';
-    word[4] = ' ';
-    word[5] = ' ';
+neww:
+    w[0] = ' ';
+    w[1] = ' ';
+    w[2] = ' ';
+    w[3] = ' ';
+    w[4] = ' ';
+    w[5] = ' ';
 again:
-    if (c == '*' || c == ' ' || c == '\t') {
+    if (c == ' ' || c == '\t') {
         goto again;
     }
     if (c == ';') {
@@ -64,39 +64,90 @@ again:
         goto again;
     }
     if (c >= 'a' && c <= '9') {
-        word[0] = c;
+        w[0] = c;
         int i = 1;
         while (*p >= 'a' && *p <= 'z' || *p >= '0' && *p <= '9') {
             if (i < 6) {
-                word[i++] = *p;
+                w[i++] = *p;
             }
             p++;
         }
         if (*p == ':') {
             p++;
-            goto labeled;
+            goto lbld;
         }
-        goto instruction;
+        goto instr;
       default:
         fail();
         return;
     }
-labeled:
-    label[0] = word[1];
-    if (word[0] > 1) {
-        label[1] = word[2];
+
+lbld:
+    label[0] = w[1];
+    if (w[0] > 1) {
+        label[1] = w[2];
     }
     // do something to define it
-    goto newword;
-instruction:
-    if (same(word, "adc   ")) {
+    goto neww;
+
+instr:
+    // Opcode-only instructions
+    w2 = simple;
+    while (*w2) {
+        flag=comp();
+        if (flag) {
+            *out++ = w2[5];
+            goto end;
+        }
+        w2 += 6;
     }
-    if (same(word, "addr  ")) {
-        // ADD A, r
+
+    // Single register instructions
+    w2="inr  ";
+    flag=comp();
+    if (flag) {
+        // possibly r_or_memory takes the bit pattern and this is a goto, b/c there's only one-byte
+        // operations?  but what about the shift amount?
+        op = r_or_memory(&p);
+        *out++ = 0x04 | (op << 3);
+        goto end;
     }
-    if (same(word, "addn  ")) {
+    w2="dcr  ";
+    flag=comp();
+    if (flag) {
+        op = rm(&p);
+        *out++ = 0x05 | (op << 3);
+        goto end;
     }
-    if (same(word, "bit   ")) {
+
+end:
+    // TODO: check that there's nothing left over
+    return p;
+}
+
+// All of these are 6 bytes: five bytes of opcode name followed by a single opcode byte,
+// to be emitted literally, followed by NUL.
+const unsigned char simple[] =
+    "cma  \x2F"
+    "cmc  \x3F"
+    "stc  \x37";
+
+// operand scanners also scan any space, a single tab is required
+
+// second operand is 8-bit Register or Memory: b,c,d,e,h,l,m,a
+// with m meaning (hl).
+int rm() {
+    if (*p != '\t') {
+        fail();
     }
-    // and so on
+    p++;
+}
+
+int comp() {
+    if (w[0] != w2[0]) return 0;
+    if (w[1] != w2[1]) return 0;
+    if (w[2] != w2[2]) return 0;
+    if (w[3] != w2[3]) return 0;
+    if (w[4] != w2[4]) return 0;
+    return 1;
 }
